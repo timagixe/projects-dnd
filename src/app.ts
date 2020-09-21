3bdaff4b9fcbdad9183c0bb2ed3490b034b1bb70
Tima Gixe <timagixe@protonmail.com>
2020-09-21 21:26:11 +0300

    Add submit listener

--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,34 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   rootElement: HTMLDivElement;
   formElement: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
-    this.templateElement = document.getElementById(
-      'project-input'
-    ) as HTMLTemplateElement;
+    this.templateElement = document.getElementById('project-input') as HTMLTemplateElement;
     this.rootElement = document.getElementById('app') as HTMLDivElement;
 
-    const importedNode = document.importNode(
-      this.templateElement.content,
-      true
-    );
+    const importedNode = document.importNode(this.templateElement.content, true);
+
     this.formElement = importedNode.firstElementChild as HTMLFormElement;
+    this.formElement.id = 'user-input';
+
+    this.titleInputElement = this.formElement.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.formElement.querySelector('#description') as HTMLInputElement;
+    this.peopleInputElement = this.formElement.querySelector('#people') as HTMLInputElement;
+
+    this.configureListener();
     this.appendToRoot(this.formElement);
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configureListener() {
+    this.formElement.addEventListener('submit', this.submitHandler.bind(this));
   }
 
   private appendToRoot(el: HTMLElement) {
