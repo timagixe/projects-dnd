2973e6ee47a58a555253bc6a2be00404caf3d9f9
Tima Gixe <timagixe@protonmail.com>
2020-09-21 20:57:59 +0300

    Render form element

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-// initial
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  rootElement: HTMLDivElement;
+  formElement: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      'project-input'
+    ) as HTMLTemplateElement;
+    this.rootElement = document.getElementById('app') as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.formElement = importedNode.firstElementChild as HTMLFormElement;
+    this.appendToRoot(this.formElement);
+  }
+
+  private appendToRoot(el: HTMLElement) {
+    this.rootElement.insertAdjacentElement('afterbegin', el);
+  }
+}
+
+const projectInput = new ProjectInput();
