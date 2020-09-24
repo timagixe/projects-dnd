df945e441f88c49de29da7af55ce59392c9981e0
Tima Gixe <timagixe@protonmail.com>
2020-09-24 12:00:23 +0300

    Remove app.ts

--- src/app.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="./components/ProjectInput.ts"/>
-/// <reference path="./components/ProjectList.ts"/>
-
-namespace App {
-  new ProjectInput();
-  new ProjectList('active');
-  new ProjectList('finished');
-}
